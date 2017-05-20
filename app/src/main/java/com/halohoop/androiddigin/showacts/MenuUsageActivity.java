22948b6284561d1a22a053534b4ae65255461122
halohoop <halohoop@163.com>
2017-05-20 16:11:21 +0800

    add commit to usage of SnackBar!

--- app/src/main/java/com/halohoop/androiddigin/showacts/MenuUsageActivity.java
+++ app/src/main/java/com/halohoop/androiddigin/showacts/MenuUsageActivity.java
@@ -1,6 +1,7 @@
 package com.halohoop.androiddigin.showacts;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,10 +16,13 @@ import com.halohoop.androiddigin.utils.Utils;
 
 public class MenuUsageActivity extends AppCompatActivity {
 
+    private CoordinatorLayout coordinar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_usage);
+        coordinar = (CoordinatorLayout) findViewById(R.id.coordinar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,8 +30,15 @@ public class MenuUsageActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Utils.log("onClick 's view is:"+view.getClass().getSimpleName());
+                //Snackbar出现的同时fab按钮会向上调整位置，以腾出空间
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Snackbar.make(coordinar, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                //Snackbar出现的同时fab按钮不会向上调整位置，不能腾出空间
+//                Snackbar.make(coordinar的父亲view，如果有的话, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
     }
